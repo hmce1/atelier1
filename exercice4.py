8c80ebb31e8bbac6d73ebdf181711c25466c270b
hmce1 <119764939+hmce1@users.noreply.github.com>
2022-12-07 12:27:04 +0000

    Update exercice4.py

--- exercice4.py
+++ exercice4.py
@@ -4,7 +4,7 @@ def ConvertBinToDic(Decimal):
     Binary = "" #on a définit binary comme une chaîne de caractères vide
     while Decimal >= 0:
         Binary += str(Decimal % 2) #append permet d'ajouter decimal % 2 dans Binary
-        Decimal = Decimal // 2 #on va diviser sur 2 et obtient le quotient jusqu'à 0
+        Decimal //= 2 #on va diviser sur 2 et obtient le quotient jusqu'à 0
         if Decimal == 0:
             break
     return Binary[::-1] #on a  slicing: [start: end: step] inverser la chaîne de caractères
